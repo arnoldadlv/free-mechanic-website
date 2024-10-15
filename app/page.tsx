bc956f1c2bbabd3af61dd06b7e089375309867d0
Arnold De La Vega <arnoldd@device-0004.local>
2024-10-14 23:16:34 -0700

    Flipping cards are functional now

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,7 @@ export default function Home() {
           </Button>
         </div>
       </div>
+
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-xl text-center justify-center">
           <span className={title()}>Services</span>
